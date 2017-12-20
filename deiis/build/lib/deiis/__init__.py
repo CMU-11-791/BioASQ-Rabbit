524725260c76a05f1eb9257e45b89d0bf7fbd5e2
Keith Suderman <suderman@cs.vassar.edu>
2017-12-19 22:17:07 -0500

    Remove deiis build artifacts

--- deiis/build/lib/deiis/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Python classes for use the in the BioASQ challenge.
-
-Classes are provided for:
-1. Serializing object to/from JSON.
-2. Managing RabbitMQ message queues.
-3. Defining the data model for classes serialized to JSON and sent of the
-message queues.
-"""
